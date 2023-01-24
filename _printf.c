7b49a8feb4ffbe3d3a2a802b4f12c73093050199
sunday0prime <sundayprime@gmail.com>
2023-01-24 04:36:58 -0800

    printf team project

--- _printf.c
+++ _printf.c
@@ -10,15 +10,16 @@
 int _printf(const char *format, ...)
 {
 	int (*pfunc)(va_list, flags_t *);
-	char *p;
+	const char *p;
 	va_list arguments;
 	flags_t flags = {0, 0, 0};
 
-	int count = 0;
+	register int count = 0;
 
 	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]) || 
-			(format[0] == '%' && format[1] == ' ' && !format[2]))
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 	for (p = format; *p; p++)
 	{
@@ -30,11 +31,12 @@ int _printf(const char *format, ...)
 				count += _putchar('%');
 				continue;
 			}
-			while(get_flag(*p, &flags))
+			while (get_flag(*p, &flags))
 				p++;
-
-			pfunc = (get_flags(*p, &flags));
-			count += (pfunc) ? pfunc(arguments, &flags) : _printf("%%%c", *p);
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
 		} else
 			count += _putchar(*p);
 	}
